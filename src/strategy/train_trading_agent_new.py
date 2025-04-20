609bfb7468ae989c6073a2a9800da0517a9cecd9
Xiaokang Wang <52538116+Mathheadkang@users.noreply.github.com>
2025-04-20 00:08:56 -0700

    Add new features

---
+++ src/strategy/train_trading_agent_new.py
@@ -0,0 +1,184 @@
+# Description: This file contains the function to train the trading agent.
+from .engine.trading_agent import DDQNAgent
+from .engine.utils_train import *
+
+import os
+from time import time
+from datetime import datetime
+
+
+from gymnasium import register
+import numpy as np
+
+import gymnasium as gym
+from gymnasium import spaces
+import torch
+from torchinfo import summary
+
+
+####################################################################################################
+# Define the training function
+def train_trading_agent(config, logger):
+    """
+    Train the trading agent.
+    """
+
+    # Get the model path
+    model_path = os.path.join(config['info']['local_data_path'], 'models')
+    os.makedirs(model_path, exist_ok=True)
+    result_path = os.path.join(config['info']['local_data_path'], 'evaluation')
+    os.makedirs(result_path, exist_ok=True)
+
+    # Get the configuration parameters
+    trading_days = config["strategy"]["trading_days"]
+    max_episodes = config["strategy"]["max_episodes"]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Get the environment
+    env = config['strategy']['environment']
+    # register the environment
+    if env == 'single':
+        register(
+            id='trading-v0',
+            entry_point='strategy.engine.trading_env:TradingEnv',
+            max_episode_steps=trading_days
+        )
+        # make the environment
+        trading_environment = gym.make('trading-v0', config=config, logger=logger)
+    elif env == 'portfolio':
+        register(
+            id='trading-port-v0',
+            entry_point='strategy.engine.trading_env_portfolio:TradingEnv',
+            max_episode_steps=trading_days
+        )
+        # make the environment
+        trading_environment = gym.make('trading-port-v0', config=config, logger=logger)
+    
+    logger.info(f'Environment: {env}')
+    seed = 42
+    trading_environment.reset(seed = seed, options=None)
+
+    # Get environment parameters
+    if isinstance(trading_environment.action_space, spaces.Discrete):
+        state_dimension = trading_environment.observation_space.shape[0]
+        flattened_state_dimension = state_dimension
+        action_dimension = trading_environment.action_space.n
+    elif isinstance(trading_environment.action_space, spaces.MultiDiscrete):
+        state_dimension = trading_environment.observation_space.shape
+        flattened_state_dimension = np.prod(state_dimension)
+        action_dimension = trading_environment.action_space.nvec
+        # If you need the total number of possible actions
+        total_actions = np.prod(action_dimension)
+    else:
+        raise ValueError(f"Unsupported action space type: {type(trading_environment.action_space)}")
+    max_episode_steps = trading_environment.spec.max_episode_steps
+
+    # Create the trading agent
+    trading_agent = DDQNAgent(
+        config=config, logger=logger, state_dimension=state_dimension, action_dimension=action_dimension, device=device
+    )
+    logger.info(summary(trading_agent.online_model, input_size=(1, flattened_state_dimension)))
+
+    # If there is a checkpoint, load it
+    # Load the latest checkpoint if it exists
+    start_episode = load_checkpoint(trading_agent, model_path, device, logger, env)
+
+
+    # Initialize the variables
+    episode_time, navs, market_navs, diffs, episode_eps = [], [], [], [], []
+
+    # Define the visualization function
+    def track_results(episode, nav_ma_100, nav_ma_10,
+                    market_nav_100, market_nav_10,
+                    win_ratio, total, epsilon):
+        time_ma = np.mean([episode_time[-100:]])
+        T = np.sum(episode_time)
+    
+        template = '{:>4d} | {} | Agent: {:>6.1%} ({:>6.1%}) | '
+        template += 'Market: {:>6.1%} ({:>6.1%}) | '
+        template += 'Wins: {:>5.1%} | eps: {:>6.3f}'
+        logger.info(template.format(episode, format_time(total), 
+                            nav_ma_100-1, nav_ma_10-1, 
+                            market_nav_100-1, market_nav_10-1, 
+                            win_ratio, epsilon))
+        
+
+    # Train the agent
+    start = time()
+    result = []
+    for episode in range(start_episode, max_episodes+1):
+        this_state = trading_environment.unwrapped.reset_trading()
+        for episode_step in range(max_episode_steps):
+            action = trading_agent.epsilon_greedy(this_state.reshape(-1, flattened_state_dimension))
+            next_state, reward, done, _ = trading_environment.unwrapped.trading_env_step(action)
+
+            # Convert next_state, reward, done to tensors on the same device
+            next_state = torch.tensor(next_state, dtype=torch.float32, device=device)
+            reward = torch.tensor([reward], dtype=torch.float32, device=device)
+            done_val = torch.tensor(0.0 if done else 1.0, dtype=torch.float32, device=device)
+
+            # Update the agent
+            trading_agent.memorize_transition(this_state, action, reward, next_state, done_val)
+            if trading_agent.train:
+                trading_agent.experience_replay()
+            if done:
+                break
+            this_state = next_state
+
+        # get DataFrame with seqence of actions, returns and nav values
+        result = trading_environment.unwrapped.simulator.result()
+    
+        # get results of last step
+        final = result.iloc[-1]
+
+        # apply return (net of cost) of last action to last starting nav 
+        nav = final.nav * (1 + final.strategy_return)
+        navs.append(nav)
+
+        # market nav 
+        market_nav = final.market_nav
+        market_navs.append(market_nav)
+
+        # track difference between agent an market NAV results
+        diff = nav - market_nav
+        diffs.append(diff)
+
+        # display results
+        if episode % 10 == 0:
+            track_results(episode, np.mean(navs[-100:]), np.mean(navs[-10:]),
+                        np.mean(market_navs[-100:]), np.mean(market_navs[-10:]),
+                        np.mean([diff > 0 for diff in diffs[-100:]]), time() - start, trading_agent.epsilon)
+        
+        # if agent has been winning for a while, stop training
+        if len(diffs) > 25 and all([r > 0 for r in diffs[-25:]]):
+            # print the tail of the result
+            logger.infor(f'The agent has been winning for a while, stop training, the tail is {result.tail()}')
+            break
+
+        # Save the checkpoint every 10 episodes
+        if episode % 10 == 0:
+            save_checkpoint(trading_agent, episode, config, model_path, navs, market_navs, diffs, logger, env)
+    
+    logger.info(f'Finished training after {format_time(time() - start)}')
+    # save the final model
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    torch.save(trading_agent.online_model.state_dict(), os.path.join(model_path, f'final_model_{env}_{timestamp}.pth'))
+    # save the final results
+    results = pd.DataFrame({
+        'Episode': list(range(1, len(navs) + 1)),
+        'Agent': navs,
+        'Market': market_navs,
+        'Difference': diffs
+    })
+    # Add rolling strategy win percentage
+    results['Strategy Wins (%)'] = (results.Difference > 0).rolling(100).sum()
+
+    results.to_csv(os.path.join(result_path, f'final_results_{env}_{timestamp}.csv'))
+    
+
+    # Close the environment
+    trading_environment.close()
+
+
+
+
+    
---
+++ src/trade_w_llm_new.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This module contains the main function of all drivers to preidct stock with news.
+Should use config/predict_stock_w_news.toml to configure the parameters.
+"""
+
+import os
+import argparse
+import errno
+import datetime
+import warnings
+
+# utils
+from utils.config_tool import parse_config, save_config_copy
+from utils.directory_tool import ensure_dir, get_directory_names
+from utils.logging_tool import initialize_logger
+
+# custom modules
+from ingestion.stock_driver import ingest_stock_data
+from feature.feature_engineer_driver import calculate_factors
+from strategy.train_trading_agent import train_trading_agent
+
+############################################
+def main(opt_params):
+		"""
+		The main function to predict stock with news.
+
+		Args:
+				opt_params: Optional parameters via argparse
+
+		Returns:
+				None
+		"""
+
+
+		# Optional parameters
+		config_filename = opt_params.config_filename
+		dir_project = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+		# Configuration
+		# Import the configuration file
+		config_file = os.path.join(dir_project, config_filename)
+		if os.path.exists(config_file):
+			config = parse_config(config_file)
+		else:
+			# Raise an error if the configuration file does not exist
+			raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_file)
+		
+		# Add today to the configuration
+		config['date']['today'] = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+
+		# Data directories
+		# Forming data directories in the local data path
+		dirs = get_directory_names(
+			path_core_data = config["info"]["local_data_path"],
+			dirs_names = config["info"]["dirs_names"]
+		)
+
+
+		# Save the config as a copy
+		ensure_dir(dirs["config_archive"])
+		save_config_copy(
+			config_path = dirs["config_archive"],
+			config = config,
+			file_name = "trade_w_llm_copy_.toml"
+		)
+
+
+		# Logging
+		# Initialize the logger
+		ensure_dir(dirs["logs"])
+		log_file = "{}_log_{}.txt".format(
+			os.path.splitext(os.path.basename(config_filename))[0],
+			config["date"]['today'])
+		logger = initialize_logger(
+			log_path = dirs["logs"],
+			log_file = log_file
+		)
+		
+		# create all the files
+		for directory in dirs.keys():
+			ensure_dir(dirs[directory])
+
+
+
+		############################################
+		# Starting pipeline
+
+	
+		if config['pipeline']['ingestion']:
+			logger.info('---------- pipeline: ingestion ----------')
+			logger.info('Start ingesting stock data...')
+			ensure_dir(dirs["data_raw"])
+
+			# Ingest stock data
+			ingest_stock_data(config, logger)
+
+			logger.info('Stock data ingestion completed.\n')
+
+
+		if config['pipeline']['feature']:
+			logger.info('---------- pipeline: feature ----------')
+			logger.info('Start calculating factors...')
+
+			# Calculate factors
+			calculate_factors(config, logger)
+
+			logger.info('Factors calculation completed.\n')
+
+		
+		if config['pipeline']['strategy']:
+			logger.info('---------- pipeline: strategy ----------')
+			logger.info('Start trading with reinforcement learning agent...')
+
+			# Trading with reinforcement learning agent
+			with warnings.catch_warnings():
+				warnings.filterwarnings("ignore", category=UserWarning)
+					
+				train_trading_agent(config, logger)
+
+			logger.info('Trading with reinforcement learning agent completed.\n')
+
+
+			
+
+
+
+
+############################################
+if __name__ == '__main__':
+		# Argument parsing
+		parser = argparse.ArgumentParser(description='Trading with LLM.')
+		parser.add_argument(
+			'--config',
+			type=str,
+			default='config/trade_w_llm.toml',
+			dest='config_filename',
+			help='the path to the configuration file'
+		)
+
+		# Parse the arguments
+		args = parser.parse_args()
+
+		# Run the main function
+		main(args)
